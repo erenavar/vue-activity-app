c006337b40a34087a3e167243385175e9bf8ab81
erenavar <43113599+erenavar@users.noreply.github.com>
2020-10-06 19:56:53 +0300

    test2

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,4 +1,8 @@
 <template>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: event.id } }"
+  >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
@@ -10,9 +14,9 @@
 <script>
 export default {
   props: {
+    event: Object,
+  },
 }
-</script>
-
 </script> 
 <style scoped>
 .event-card {
